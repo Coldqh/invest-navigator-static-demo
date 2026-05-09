e21c32e21c6fb888ff37e6e05ff52c73bad44f3a
Coldqh <pahomov.0666@gmail.com>
2026-05-09 15:53:21 +0300

    Fix GitHub Pages dist deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-function resolveBase() {
-    const repository = process.env.GITHUB_REPOSITORY?.split("/")[1];
-
-    if (process.env.GITHUB_ACTIONS === "true" && repository) {
-        return `/${repository}/`;
-    }
-
-    return "/";
-}
-
 export default defineConfig({
-    base: resolveBase(),
+    base: "/invest-navigator-static-demo/",
     plugins: [react()],
     server: {
         host: "0.0.0.0",
